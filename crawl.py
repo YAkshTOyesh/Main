70623656461601b8cbdd01215a554f07e8455eb7
Seongin Cho <s19kiki7226@gmail.com>
2023-11-04 19:27:34 -0500

    Crawling added

---
+++ crawl.py
@@ -0,0 +1,42 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import time
+import urllib.request
+
+driver = webdriver.Chrome()
+driver.get("https://www.google.co.kr/imghp?hl=ko&tab=wi&authuser=0&ogbl")
+elem = driver.find_element(By.NAME, "q")
+elem.send_keys("Shades of red") #Name for search
+elem.send_keys(Keys.RETURN)
+
+SCROLL_PAUSE_TIME = 1
+last_height = driver.execute_script("return document.body.scrollHeight")
+while True:
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    time.sleep(SCROLL_PAUSE_TIME)
+    new_height = driver.execute_script("return document.body.scrollHeight")
+    if new_height == last_height:
+        try:
+            driver.find_element(By.CSS_SELECTOR, ".LZ4I").click()
+        except:
+            break
+    last_height = new_height
+
+images = driver.find_elements(By.CSS_SELECTOR, ".rg_i.Q4LuWd")
+images_subset = images[0:200] 
+count = 1
+for image in images_subset:
+    try:
+        image.click()
+        time.sleep(2)
+        imgUrl = driver.find_element(By.XPATH, '//*[@id="Sva75c"]/div[2]/div[2]/div[2]/div[2]/c-wiz/div/div/div/div[3]/div[1]/a/img[1]').get_attribute("src")
+        opener=urllib.request.build_opener()
+        opener.addheaders=[('User-Agent','Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1941.0 Safari/537.36')]
+        urllib.request.install_opener(opener)
+        urllib.request.urlretrieve(imgUrl, str(count) + ".jpg")
+        count = count + 1
+    except Exception as e:
+        print(e)
+
+driver.close()
